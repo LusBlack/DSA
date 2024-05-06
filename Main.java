b298e3cbfacf597510644302d323e6a37945f9fe
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-05-06 01:21:16 +0100

    stacks in java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+
+        System.out.println(stack.empty());
+
+        stack.push("Minecraft");
+        stack.push("Final Fanatasy");
+        
+    }
+}
+
