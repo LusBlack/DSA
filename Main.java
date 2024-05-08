b8fe0ccdf1d97a4b44acecc8811a4581d929033b
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-05-08 09:10:12 +0100

    stacks in java

--- Main.java
+++ Main.java
@@ -2,7 +2,25 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-       
+       Stack<String> stack = new Stack<String>();
+
+       System.out.println(stack.empty());
+
+       stack.push("Minecraft");
+       stack.push("Call of duty");
+       stack.push("Blade");
+       stack.push("Battlefield 3");
+
+       System.out.println(stack);
+
+       String myFavGame = stack.pop();
+
+       System.out.println(myFavGame);
+
+
+       System.out.println(stack);
+
+
     }
 
 }
