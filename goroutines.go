29e10848b82284337cb4e822a234fff82dba2674
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-05-08 08:52:21 +0100

    still on goroutines

--- goroutines.go
+++ goroutines.go
@@ -2,19 +2,58 @@ package pkg
 
 import (
 	"fmt"
-	"runtime"
+	"log"
+	"time"
 )
 
 var p = fmt.Println
 
 func main() {
-	p("main execution started")
-	p("No. of CPUs:", runtime.NumCPU())
-	p("No. of Goroutines:", runtime.NumGoroutine())
 
-	p("OS:", runtime.GOOS)
-	p("Arch:", runtime.GOARCH)
+	id := getUserByName("John")
+	p(id)
 
-	p("GOMAXPROCS", runtime.GOMAXPROCS(0))
+	chats := getUserChats(id)
+	friends := getUserFriends(id)
+
+	log.Println(chats)
+	log.Println(friends)
 
 }
+
+func getUserFriends(id string) []string {
+
+	return []string{
+		"John",
+		"Jane",
+		"Joe",
+		"Tiago",
+		"James",
+	}
+}
+
+func getUserChats(id string) []string {
+	time.Sleep(time.Second * 2)
+
+	return []string{
+		"John",
+		"Jane",
+		"Joe",
+	}
+
+}
+
+func getUserByName(name string) string {
+	time.Sleep(time.Second * 1)
+
+	return fmt.Sprintf("%s-2", name)
+}
+
+// p("main execution started")
+// p("No. of CPUs:", runtime.NumCPU())
+// p("No. of Goroutines:", runtime.NumGoroutine())
+
+// p("OS:", runtime.GOOS)
+// p("Arch:", runtime.GOARCH)
+
+// p("GOMAXPROCS", runtime.GOMAXPROCS(0))
