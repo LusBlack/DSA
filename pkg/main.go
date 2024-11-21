4f1e1f70d4d2da69c7d75b9bea271de070a48892
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-11-21 10:25:22 +0100

    checking out go's runtime

--- pkg/main.go
+++ pkg/main.go
@@ -51,4 +51,13 @@ func main() {
 	myQueue.Enqueue(300)
 	myQueue.Deqeue()
 
+	p("main execution started")
+	p("No. of CPUs:", runtime.NumCPU())
+	p("No. of Goroutines:", runtime.NumGoroutine())
+
+	p("OS:", runtime.GOOS)
+	p("Arch:", runtime.GOARCH)
+
+	p("GOMAXPROCS", runtime.GOMAXPROCS(0))
+
 }
